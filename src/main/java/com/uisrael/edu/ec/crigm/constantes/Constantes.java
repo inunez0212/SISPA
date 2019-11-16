364f574a1bd4f3abca899d0045b599298b430705
kali <kali@kali.casa>
2019-11-15 19:26:07 -0500

    Tema modena

---
+++ src/main/java/com/uisrael/edu/ec/crigm/constantes/Constantes.java
@@ -0,0 +1,6 @@
+package com.uisrael.edu.ec.crigm.constantes;
+
+public final class Constantes {
+	
+	
+}
---
+++ src/main/java/com/uisrael/edu/ec/sispa/SISPAApplication.java
@@ -0,0 +1,24 @@
+package com.uisrael.edu.ec.sispa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.filter.RequestContextFilter;
+
+@SpringBootApplication
+@ComponentScan(basePackages = "com.uisrael.edu.ec.crigm") 
+
+public class SISPAApplication{
+
+	@Bean
+	public RequestContextFilter requestContextFilter() {
+		return new RequestContextFilter();
+	}
+	
+	public static void main(String[] args)  {
+		SpringApplication.run(SISPAApplication.class, args);
+	}
+
+  
+}
